87edc628902ce95ae44ec78142cc90f3a1916624
Peter Uithoven <peter@peteruithoven.nl>
2019-05-29 18:07:59 +0200

    Using custom tick

--- src/OrdersHistoryGraph.js
+++ src/OrdersHistoryGraph.js
@@ -11,9 +11,34 @@ import {
   Brush,
   ReferenceLine,
 } from 'recharts';
-
+import dayjs from 'dayjs';
 import { blueGrey, teal } from '@material-ui/core/colors';
 import * as formatters from './formatters.js';
+
+const renderYearTick = tickProps => {
+  const { x, y, payload } = tickProps;
+  const { value, offset } = payload;
+  const date = dayjs(value);
+  const label = date.format('YYYY');
+  const month = date.month();
+
+  if (month === 6) {
+    return (
+      <text x={x + offset} y={y - 4} textAnchor="middle">
+        {label}
+      </text>
+    );
+  }
+
+  //   const isLast = month === 11;
+
+  if (month === 0) {
+    const pathX = Math.floor(x) + 0.5;
+
+    return <path d={`M${pathX},${y - 4}v${-35}`} stroke="red" />;
+  }
+  return null;
+};
 
 function OrdersHistoryGraph({ data, fields }) {
   return (
@@ -21,8 +46,21 @@ function OrdersHistoryGraph({ data, fields }) {
       <LineChart data={data} margin={{ top: 0, right: 30, left: 0, bottom: 0 }}>
         <XAxis
           dataKey="open_for_orders_date"
-          tickFormatter={formatters.shortDate}
+          tickFormatter={formatters.month}
         />
+        <XAxis
+          dataKey="open_for_orders_date"
+          axisLine={false}
+          tickLine={false}
+          interval={0}
+          tick={renderYearTick}
+          //   tickFormatter={formatters.year}
+          //   height={1}
+          //   scale="band"
+          xAxisId="year"
+          //   allowDuplicatedCategory={false}
+        />
+
         <YAxis />
         <CartesianGrid strokeDasharray="3 5" />
         <Tooltip
--- src/formatters.js
+++ src/formatters.js
@@ -4,3 +4,5 @@ export const toPercent = (decimal, fixed = 0) =>
   `${(decimal * 100).toFixed(fixed)}%`;
 export const shortDate = date => dayjs(date).format('MMM YY');
 export const longDate = date => dayjs(date).format('D MMM YYYY');
+export const year = date => dayjs(date).format('YYYY');
+export const month = date => dayjs(date).format('MMM');
